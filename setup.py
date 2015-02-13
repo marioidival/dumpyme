6f8ab418f198a438829408dbfefe29d9f6eea732
Mario Idival <marioidival@gmail.com>
2015-02-13 01:46:31 -0300

    0.0.1 version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 root = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(root, 'README.md')) as f:
+with open(os.path.join(root, 'README.rst')) as f:
     README = f.read()
 
 setup(
     name="dumpyme",
-    version="0.0.0",
+    version="0.0.1",
     author="Mario Idival",
     author_email="marioidival@gmail.com",
     description=("Command line package to get dumps"),
@@ -17,7 +17,7 @@ setup(
     keywords="dump dumpyme mongo postgres mysql",
     url="https://github.com/marioidival/dumpyme",
     packages=['src'],
-    install_requires=[],
+    install_requires=['click', 'fabric'],
     tests_require=['nose', 'coverage'],
     test_suite='tests',
     entry_points='''
