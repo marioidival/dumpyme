ab4446e494764fd340ccf72b56d4fa20dab31bc5
Mario Idival <marioidival@gmail.com>
2015-03-17 23:49:53 -0300

    example to apply

--- src/utils/supported_databases.py
+++ src/utils/supported_databases.py
@@ -10,5 +10,15 @@ dump_commands = {
     "mongodb": "mongodump --db {0} --out {1}",
 }
 
+"""
+POSTGRESQL : pg_dump -d database -U user/owner > file.sql
+    variações:
+        --table=nome_da_tabela
+        --table=nome_da_tabela --table=nome_da_outra_tabela
+
+        --exclude=nome_da_tabela
+        --exclude=nome_da_tabela --exclude=nome_da_outra_tabela
+"""
+
 # const with list of supported database types
 SUPPORTED = dump_commands.keys()
